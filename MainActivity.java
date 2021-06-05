7b50687722fb6a155d23b73c933db1a89d2d3b1c
Tijani-Mubarak2001 <66437072+Tijani-Mubarak2001@users.noreply.github.com>
2021-06-05 18:40:24 +0100

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,106 @@
+package com.tijaniadewale.temp.eggtimer;
+
+import android.media.MediaPlayer;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    SeekBar timerSeekBar;
+    TextView timerTextView;
+    Button ControllerButton;
+    Boolean counterIsActive = false;
+    CountDownTimer countDownTimer;
+
+
+    public void resetTimer(){
+        timerTextView.setText("0:30");
+        timerSeekBar.setProgress(30);
+        countDownTimer.cancel();
+        ControllerButton.setText("Go!");
+        timerSeekBar.setEnabled(true);
+        counterIsActive = false;
+    }
+
+
+    
+    public void updateTimer(int secondsLeft){
+        int minutes = (int) secondsLeft / 60;
+        int seconds = secondsLeft - (minutes * 60);
+
+        String secondString = Integer.toString(seconds);
+
+        if(seconds <= 9){
+            secondString = "0" + secondString;
+        }
+
+        timerTextView.setText(Integer.toString(minutes) + ":" + secondString );
+    }
+    
+    public void controlTimer(View view){
+
+        if(counterIsActive == false) {
+            counterIsActive = true;
+            timerSeekBar.setEnabled(false);
+            ControllerButton.setText("Stop");
+
+            countDownTimer = new CountDownTimer(timerSeekBar.getProgress() * 1000 + 100, 1000) {
+
+                @Override
+                public void onTick(long millisUntilFinished) {
+
+                    updateTimer((int) millisUntilFinished / 1000);
+
+                }
+
+                @Override
+                public void onFinish() {
+
+                    resetTimer();
+                    MediaPlayer mplayer = MediaPlayer.create(getApplicationContext(), R.raw.airhorn);
+                    mplayer.start();
+
+                }
+            }.start();
+        }else{
+            resetTimer();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        timerSeekBar = (SeekBar) findViewById(R.id.timerSeekBar);
+
+        timerTextView = (TextView) findViewById(R.id.timerTextView);
+        ControllerButton = (Button) findViewById(R.id.ControllerButton);
+
+
+        timerSeekBar.setMax(600);
+        timerSeekBar.setProgress(30);
+
+        timerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                
+                updateTimer(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}
